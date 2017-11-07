8bca7d3a7df37125ce12ca6d9383160aefb94274
Basti Tee <basti.tee@posteo.de>
2017-11-07 14:05:49 +0100

    update

--- generate-readme.sh
+++ generate-readme.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cd "$( dirname "$( readlink -f "$0" )" )"
 {
-find -maxdepth 1 -type d | grep -v -e "^\.$" -e ".git" | sort -r |\
-while read dir
-do
-    bn=$( basename $dir )
-    echo -e "# $bn\n"
-    find $bn -type f | awk '{ print length($0) " " $0; }' $file | sort -n |\
-    cut -d ' ' -f 2- | while read file
+    find -maxdepth 1 -type d | grep -v -e "^\.$" -e ".git" | sort -r |\
+        while read dir
     do
-        echo " - [$( basename $file )]($file)"
+        bn=$( basename $dir )
+        echo -e "# $bn\n"
+        find $bn -type f | awk '{ print length($0) " " $0; }' $file | sort -n |\
+            cut -d ' ' -f 2- | while read file
+        do
+            echo " - [$( basename $file )]($file)"
+        done
+        echo -e "\n---\n"
     done
-    echo -e "\n---\n"
-cat << EOF
+    cat << EOF
 All files in this repository are release snapshots of [d3-workbench](https://github.com/BastiTee/d3-workbench). See respective [license file](https://github.com/BastiTee/d3-workbench/LICENSE) inside the source repository.
 EOF
-done
 } > README.md
 cat README.md
